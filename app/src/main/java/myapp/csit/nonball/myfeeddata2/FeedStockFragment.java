6f21d40717cdb9e75ec13b4bde620534a0391457
harzancs <592021212@tsu.ac.th>
2020-01-16 09:57:20 +0700

    zan 16012563 0957

--- app/src/main/java/myapp/csit/nonball/myfeeddata2/FeedStockFragment.java
+++ app/src/main/java/myapp/csit/nonball/myfeeddata2/FeedStockFragment.java
@@ -3,11 +3,17 @@ package myapp.csit.nonball.myfeeddata2;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 /**
@@ -15,6 +21,7 @@ import android.view.ViewGroup;
  */
 public class FeedStockFragment extends Fragment {
 
+    private RecyclerView mRecyclerView;
 
     public FeedStockFragment() {
         // Required empty public constructor
@@ -25,7 +32,54 @@ public class FeedStockFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed_stock, container, false);
+
+        View _view = inflater.inflate(R.layout.fragment_feed_stock, container, false);
+        mRecyclerView = (RecyclerView) _view.findViewById(R.id.recycleview);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecyclerView.setAdapter(new CustomReclyclerView());
+
+        return _view;
     }
 
+    private class CustomReclyclerView extends RecyclerView.Adapter<ViewHolder> {
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View _view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_stock, parent, false);
+
+            return new ViewHolder(_view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            holder.title.setText("CSIT TSU");
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 10;
+        }
+    }
+
+    private class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+        ImageView imgYoutube;
+        Button edit;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            title = itemView.findViewById(R.id.nameTV);
+            imgYoutube = itemView.findViewById(R.id.imageProductIMV);
+            edit = itemView.findViewById(R.id.editBTN);
+
+            edit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
+    }
 }
